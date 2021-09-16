ae2858a2e90182286b16318f9e9e5dd1e4c1679b
lohade <abhisheklohade8584@gmail.com>
2021-09-16 12:26:24 +0530

    total wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,24 +2,29 @@ public class EmployeeWage {
     public static  final int IS_PART_TIME=1;
     public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOURS=20;
+    public static final int NUM_OF_WORKING_DAYS=20;
 
     public static void main(String[] args) {
 	// write your code here
 
         int empHrs=0;
-        int empWage=0;
-        int empCheck=(int)Math.floor(Math.random()*10) % 3;
-        switch (empCheck) {
-            case IS_PART_TIME:
-                empHrs = 4;
-            case  IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            default:
-                empHrs = 0;
+        int empWage=0,totalWage=0;
+        for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
 
+            }
+            empWage = empHrs * EMP_RATE_PER_HOURS;
+            totalWage += empWage;
+            System.out.println("emp wage:" + empWage);
         }
-        empWage= empHrs * EMP_RATE_PER_HOURS;
-        System.out.println("emp wage:" +empWage);
+        System.out.println("total wage:" + totalWage);
     }
 }
