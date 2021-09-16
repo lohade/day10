fb27a36eec7819a88346d5b16dcf0974def8b968
lohade <abhisheklohade8584@gmail.com>
2021-09-16 16:26:58 +0530

    handling multiple company

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,14 +1,12 @@
 public class EmployeeWage {
     public static  final int IS_PART_TIME=1;
     public static final int IS_FULL_TIME=2;
-    public static final int EMP_RATE_PER_HOURS=20;
-    public static final int NUM_OF_WORKING_DAYS=20;
-    public static final int MAX_HRS_IN_MONTH=100;
 
 
-    public static int computeEmpwage(){
+
+    public static int computeEmpwage(String company,int EMP_RATE_PER_HOURS,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH){
         int empHrs=0;
-        int empWage,totalWage=0,totalWorkingDays=0;
+        int totalWorkingDays=0;
         int totalEmpHrs=0;
 
         while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS) {
@@ -35,7 +33,10 @@ public class EmployeeWage {
     }
     public static void main(String[] args) {
 	// write your code here
-        computeEmpwage();
+        computeEmpwage("techM",20,15,10);
+        computeEmpwage("Atos",15,20,15);
+
+
 
 
     }
