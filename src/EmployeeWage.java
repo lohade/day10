52e093629713944e8f3e159499b316e5e94618f7
lohade <abhisheklohade8584@gmail.com>
2021-09-16 11:54:15 +0530

    employee wage proprogram

---
+++ src/EmployeeWage.java
@@ -0,0 +1,13 @@
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=(int)Math.floor(Math.random()*10) %2;
+        if (a == 1) {
+                System.out.println("Employee is present");
+            }
+            else {
+                System.out.println("employee is absent");
+            }
+    }
+}
