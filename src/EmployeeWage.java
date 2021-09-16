47d22ce50daf1a776c6cb719da5abcf8dcf34de8
lohade <abhisheklohade8584@gmail.com>
2021-09-16 12:37:18 +0530

    calculate monthly wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,13 +3,17 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOURS=20;
     public static final int NUM_OF_WORKING_DAYS=20;
+    public static final int MAX_HRS_IN_MONTH=100;
 
     public static void main(String[] args) {
 	// write your code here
 
         int empHrs=0;
-        int empWage=0,totalWage=0;
-        for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+        int empWage,totalWage=0,totalWorkingDays=0;
+        int totalEmpHrs=0;
+
+        while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -21,10 +25,11 @@ public class EmployeeWage {
                     empHrs = 0;
 
             }
-            empWage = empHrs * EMP_RATE_PER_HOURS;
-            totalWage += empWage;
-            System.out.println("emp wage:" + empWage);
+
+            totalEmpHrs+=empHrs;
+            System.out.println("day#: " +totalWorkingDays + "emp hr:" +empHrs);
         }
-        System.out.println("total wage:" + totalWage);
+        int totalEmpWage=totalEmpHrs * EMP_RATE_PER_HOURS;
+        System.out.println("total wage:" + totalEmpWage);
     }
 }
